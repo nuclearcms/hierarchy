6e4e2037c646c8ab36101ab8c5c5c14f0f1e50a3
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-11-14 11:23:37 +0200

    Node name unique index

--- src/Providers/HierarchyServiceProvider.php
+++ src/Providers/HierarchyServiceProvider.php
@@ -8,7 +8,7 @@ use Nuclear\Hierarchy\Cache\Accessor;
 
 class HierarchyServiceProvider extends ServiceProvider {
 
-    const version = '1.0.3';
+    const version = '1.0.4';
 
     /**
      * Register the service provider.
