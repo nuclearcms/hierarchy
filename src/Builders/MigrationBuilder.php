367066eb22ced5267ef117b0f86fded61d4172db
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-12-29 20:20:19 +0200

    Small fixes

--- src/Builders/MigrationBuilder.php
+++ src/Builders/MigrationBuilder.php
@@ -64,7 +64,7 @@ class MigrationBuilder implements MigrationBuilderContract, WriterContract {
             'slug'     => 'string',
             'tag'      => 'text',
             'password' => 'string',
-            'datetime' => 'timestamp'
+            'date'     => 'timestamp'
         ]);
 
         $this->defaultType = config('hierarchy.default_type', 'string');
