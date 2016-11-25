40dc8d5680cc458a62acb2bfc23fd9b3ca3f674f
Egemen Berker Kızılcan <Hyleeh@users.noreply.github.com>
2016-11-25 11:56:39 +0200

    Disabled autocomplete

--- src/Http/Forms/NodeSourceForm.php
+++ src/Http/Forms/NodeSourceForm.php
@@ -10,11 +10,13 @@ class NodeSourceForm extends Form {
     public function buildForm()
     {
         $this->add('title', 'text', [
-            'rules' => 'required|max:255'
+            'rules' => 'required|max:255',
+            'attr' => ['autocomplete' => 'off']
         ]);
         $this->add('node_name', 'slug', [
             'rules' => 'max:255|alpha_dash|unique:node_sources,node_name',
-            'help_block' => ['text' => trans('hints.slug')]
+            'help_block' => ['text' => trans('hints.slug')],
+            'attr' => ['autocomplete' => 'off']
         ]);
     }
 
