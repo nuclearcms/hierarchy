9d02eee285adfaba36901f1ca60d97e339af507c
Egemen Berker Kızılcan <egemen@kenarkose.com>
2016-11-27 12:22:57 +0300

    Updated dependencies
    Since doctrine/annotations 1.3.0 and onwards require php 5.6, this
    would make the package 5.5 incompliant

--- src/Providers/HierarchyServiceProvider.php
+++ src/Providers/HierarchyServiceProvider.php
@@ -9,7 +9,7 @@ use Nuclear\Hierarchy\Cache\Accessor;
 
 class HierarchyServiceProvider extends ServiceProvider {
 
-    const version = '2.3.6';
+    const version = '2.3.7';
 
     /**
      * Indicates if loading of the provider is deferred.
